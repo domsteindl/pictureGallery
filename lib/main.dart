0bc008ebdfd768bf7e1c9fc45c6df8a24f3001c4
Dominik Steindl <dominik.steindl@gmx.de>
2025-09-24 11:28:48 +0200

    added home and aboutMe Screens and make the NavigationBar functional

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_picture_gallery/about_me_screen.dart';
+import 'package:flutter_picture_gallery/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
 
+class _MainAppState extends State<MainApp> {
+  final List<Widget> screens = [HomeScreen(), AboutMeScreen()];
+  final String screenTitle = '';
+  int index = 0;
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: index,
+          onDestinationSelected: (value) => setState(() {
+            index = value;
+          }),
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.image), label: 'Bilder'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Über mich'),
+          ],
         ),
+
+        appBar: AppBar(title: Text(screenTitle)),
+        body: IndexedStack(index: index, children: screens),
       ),
     );
   }
